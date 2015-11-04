629be60eaffa3e3d53a8db7b90bc43ce925db48f
Anushavan Melkonyan <Anushavan.Melkonyan@haw-hamburg.de>
2015-11-04 18:46:24 +0100

    neues git

--- TechnischeInformatikRepository/Technische Informatik Semester 2/src/Aufgabenblatt2/DoubleDoubleZuDouble.java
+++ TechnischeInformatikRepository/Technische Informatik Semester 2/src/Aufgabenblatt2/DoubleDoubleZuDouble.java
@@ -4,6 +4,7 @@ package Aufgabenblatt2;
 /** Ein Funktionales Interface für "DoubleDoubleZuDouble" 
  * die eine Methode werteAus() besitzt */
 
+
 @FunctionalInterface
 public interface DoubleDoubleZuDouble {
 	public double werteAus(double wert1, double wert2);
