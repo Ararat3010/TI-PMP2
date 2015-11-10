66693a308eea12663c0f7729dcb93ef52306cc9b
Anushavan Melkonyan <Anushavan.Melkonyan@haw-hamburg.de>
2015-11-10 02:01:04 +0100

    Bennet schau dir mal Streams an, hab extra im Quellcode kommentare hinterlassen für dich. Bitte um Rückmeldung via. Whatssapp or something other

--- TechnischeInformatikRepository/Technische Informatik Semester 2/src/Aufgabenblatt2/Streams.java
+++ TechnischeInformatikRepository/Technische Informatik Semester 2/src/Aufgabenblatt2/Streams.java
@@ -1,38 +1,76 @@
 package Aufgabenblatt2;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/** Eine Klasse die mit Streams arbeitet 
+/**
+ * Eine Klasse die mit Streams arbeitet
  * 
  * @author Anushavan Melkonyan (Anushavan.Melkonyan@haw-hamburg.de)
  *
- * 			Aufgabe: Aufgabenblatt 2, Aufgabe 2			
+ *         Aufgabe: Aufgabenblatt 2, Aufgabe 2
  */
 
 public class Streams {
 
+	/** 
+	 * Version 2.0 läuft nicht 
+	 * 
+	 * Könntest du dir mal anschauen Bennet woran das liegt, dass 
+	 * die Test fehlschlagen obwohl alles stimmt "was er erwartet und erhält 
+	 * ist indentisch....
+	 * 
+	 * 
+	 * @param text
+	 * @return
+	 */
+	
+	
+	public List<String> verarbeitungVonStrings(String text){
+		
+		String[] listArray = {text};
+		List<String> list = Arrays.asList(listArray);
+		List<String> listStream = list.stream().filter(s -> s != null)
+									.map(s -> s.trim()).map(s -> s.toUpperCase())
+									.map(s->s.replaceAll("Ä", "AE")
+											 .replaceAll("Ö", "OE")
+											 .replaceAll("Ü", "UE")
+										 	 .replaceAll("ß", "SS"))
+									.map(s -> {
+										if (s.length() > 8) {
+											return s.substring(0, 8);
+										}
+										return s;
+
+									}).collect(Collectors.toList());
+		return listStream;
+											
+		
+		
+	}
+	
+	/** 
+	 * Aktuelle Version 1.0 die läuft ohne Methode
+	 */
+	
+	
 	String[] listArray = { "Eingabe", "Aüßeres", null, "Strassen-Feger",
-			"ein Haus" };
+							"ein Haus" };
 	List<String> list = Arrays.asList(listArray);
-	List<String> listStream = list.stream()
-			.filter(s -> s != null)
-			.map(s -> s.trim())
-			.map(s -> s.toUpperCase())
-			.map(s -> s.replaceAll("Ä", "AE"))
-			.map(s -> s.replaceAll("Ö", "OE"))
-			.map(s -> s.replaceAll("Ü", "UE"))
-			.map(s -> s.replaceAll("ß", "SS"))
-			.map(s->{
-				if(s.length() > 8){
-				return s.substring(0, 8);
-				}
-				return s;
-				
-			})
-			.collect(Collectors.toList());
+	List<String> listStream = list.stream().filter(s -> s != null)
+								.map(s -> s.trim()).map(s -> s.toUpperCase())
+								.map(s->s.replaceAll("Ä", "AE")
+										 .replaceAll("Ö", "OE")
+										 .replaceAll("Ü", "UE")
+									 	 .replaceAll("ß", "SS"))
+								.map(s -> {
+									if (s.length() > 8) {
+										return s.substring(0, 8);
+									}
+									return s;
+
+								}).collect(Collectors.toList());
 
 }
--- TechnischeInformatikRepository/Technische Informatik Semester 2/src/Aufgabenblatt2/StreamsTest.java
+++ TechnischeInformatikRepository/Technische Informatik Semester 2/src/Aufgabenblatt2/StreamsTest.java
@@ -17,11 +17,27 @@ public class StreamsTest {
 	@Test
 	public void testStreams() {
 		Streams test = new Streams();
+	
+/**
+ * Test für Version 2.0 läuft nicht siehe Kommentar bei Version 2.0 Streams
+ */
 
+		
+		//assertEquals(test.verarbeitungVonStrings("Eingabe "), "EINGABE");
+		//assertEquals(test.verarbeitungVonStrings("Äußeres "), "AUESSERE");
+		//assertEquals(test.verarbeitungVonStrings("Strassen-Feger"), "STRASSEN");
+		//assertEquals(test.verarbeitungVonStrings(" ein Haus"), "EIN HAUS");
+		
+/** 
+ * Test für die Version 1.0		
+ */
+		
+		
 		assertEquals(test.listStream.get(0), "EINGABE");
 		assertEquals(test.listStream.get(1), "AUESSERE");
 		assertEquals(test.listStream.get(2), "STRASSEN");
 		assertEquals(test.listStream.get(3), "EIN HAUS");
+	
 	}
 
 }
